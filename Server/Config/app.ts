e7a5631039b81569fd849b468d88e756c5c7ee59
LAPTOP-T21LK98P\KHS <https://github.com/Hosae-Coding>
2022-06-07 12:22:28 -0400

    success to connect with mongodb

--- Server/Config/app.ts
+++ Server/Config/app.ts
@@ -4,11 +4,28 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
+import mongoose from 'mongoose';
+
 import indexRouter  from '../Routes/index';
 
 
 const app = express();
 
+// Step 2 - Complete the DB 
+import * as DBConfig from './db';
+mongoose.connect(DBConfig.LocalURI);
+const db = mongoose.connection; 
+
+// Step 3 - Listen for Connections or Errors
+db.on("open", function()
+{
+  console.log(`Connected to MongoDB at: ${DBConfig.HostName}`);
+});
+
+db.on("error", function()
+{
+  console.error(`Connection Error`);
+});
 // view engine setup
 app.set('views', path.join(__dirname, '../Views'));
 app.set('view engine', 'ejs');
